b821eeb51516d97020c1da6a585d8cce377a1783
carsten.rehfeld <carsten.rehfeld@hypoport.de>
2014-05-21 16:51:54 +0200

    Micha hat die Altlasten entfernt

--- src/org/coopmobil/ships/Rule.java
+++
@@ -1,7 +0,0 @@
-package org.coopmobil.ships;
-
-public interface Rule {
-	public CellState getStateAfteClick(int horizontalCoordinate,int verticalCoordinate,FieldModel fieldModel)
-	throws ClickNotAllowedException;
-	
-}
--- src/org/coopmobil/ships/opponent/PlayType.java
+++
@@ -1,8 +0,0 @@
-package org.coopmobil.ships.opponent;
-
-/**
- * Created by Michael on 09.02.14.
- */
-public enum PlayType {
-    SIMPLE;
-}
